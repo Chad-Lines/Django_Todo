71ffb2fa49779b7948ce5d3e151457decdb27994
Chad <chad.lines1@gmail.com>
2020-08-05 13:11:15 -0600

    2.0 Routing, Models, Templates, Forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    return render(request, 'tasks/list.html', {'tasks': tasks, 'form': form})
