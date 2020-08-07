ead0cfc82cc03e0009ddd18c3017a5a306dcb4ca
Chad <chad.lines1@gmail.com>
2020-08-07 15:20:41 -0600

    3.1 Update an Item

--- tasks/views.py
+++ tasks/views.py
@@ -15,3 +15,15 @@ def index(request):
         return redirect('/')
 
     return render(request, 'tasks/list.html', {'tasks': tasks, 'form': form})
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request, 'tasks/update_task.html', {'form': form})
