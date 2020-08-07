487b1ed7ada26050bd19c5aa18b56ee3d566291b
Chad <chad.lines1@gmail.com>
2020-08-07 15:09:21 -0600

    3.1 Create an Item

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -7,4 +7,11 @@ from .forms import *
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     return render(request, 'tasks/list.html', {'tasks': tasks, 'form': form})
